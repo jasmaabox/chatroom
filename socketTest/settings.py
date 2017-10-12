2f1fd86212a1dfdc17b771ea2c9c50366b18f984
jasmaa <jasonmaa3955@gmail.com>
2017-10-11 23:51:44 -0400

    Add pg stuff

--- socketTest/settings.py
+++ socketTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1^g_pe&bip_549f2)_lo2acnwbqud$el#!6@gx(2egg@2%9ft)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'socketTest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatterroom_db',
+        'USER': 'qbowl_admin',
+        'PASSWORD': 'robertburns',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
