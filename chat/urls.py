46e19bc8bd71b67eb48b88d67e9fc36c11153e77
jasmaa <jasonmaa3955@gmail.com>
2017-10-23 20:11:17 -0400

    Fix room names

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<label>[^/]+)/$', views.chat_room, name='chat_room'),
+    url(r'^(?P<label>^[a-z0-9_-]+)/$', views.chat_room, name='chat_room'),
 ]
