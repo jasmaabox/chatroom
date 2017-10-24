f0fde06af259b5cd5963e25c41b5cd517f909e11
prchandr <happyprems@gmail.com>
2017-10-24 10:21:43 -0400

    Adds a minimum value for color

--- chat/utils.py
+++ chat/utils.py
@@ -3,6 +3,13 @@ import re
 def fix_color(s):
     p = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', s)
     if p:
-        return s
+        h = s.lstrip('#')
+        rgbValue = 0
+        for i in [0, 2, 4]:
+            rgbValue+=int(h[i:i+2], 16)
+        if rgbValue > 64:   #Minimum color value
+            return s
+        else:
+            return '#00ff00'
     else:
         return '#00ff00'
