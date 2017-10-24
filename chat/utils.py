e7126f81e9a3726b5261509aaf231bb17c9bb8cf
prchandr <happyprems@gmail.com>
2017-10-24 10:33:34 -0400

    Made a separate function
    Moved stuff around to make it more elegant

--- chat/utils.py
+++ chat/utils.py
@@ -2,14 +2,14 @@ import re
 
 def fix_color(s):
     p = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', s)
-    if p:
-        h = s.lstrip('#')
-        rgbValue = 0
-        for i in [0, 2, 4]:
-            rgbValue+=int(h[i:i+2], 16)
-        if rgbValue > 64:   #Minimum color value
-            return s
-        else:
-            return '#00ff00'
+    if p and get_rgb_value(s) > 64:
+        return s
     else:
         return '#00ff00'
+    
+def get_rgb_value(s):
+    h = s.lstrip('#')
+    rgb_value = 0
+    for i in [0, 2, 4]:
+        rgb_value+=int(h[i:i+2], 16)
+    return rgb_value
